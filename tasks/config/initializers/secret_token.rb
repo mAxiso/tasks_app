7db66a9c93b0ec2a2ee5c6514ebf59a4750e497f
Alex Mejía <maxelmejia@hotmail.com>
2012-05-15 11:05:57 -0600

    creado proyecto en rails

---
+++ tasks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '1fad658d8ea1063cbe52a8a42393267b1c99358a0f9615ce2505948388308cbb141214198406d8be12f82e73092b8777d45936634d7e8b7cefa59fc079b6b409'
